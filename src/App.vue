67df407a2132f5e4ef84bf0ac29f3dfe4cae31e6
Obaayaa-1988 <ashbella333@gmail>
2023-04-05 15:34:39 +0000

    commented out carousel slider

--- src/App.vue
+++ src/App.vue
@@ -95,7 +95,7 @@
     <!--------------main flex------------end-->
     <!--------------review and rating flex----start-->
     <ReviewPage class="rev" @review-submitted="addReview" />
-    <CarouselSlider  />
+    <!-- <CarouselSlider  /> -->
    
     <!--------------review and rating flex----end-->
     
@@ -105,13 +105,13 @@
 
 <script>
 import ReviewPage from './components/ReviewPage.vue';
-import CarouselSlider from './components/CarouselSlider.vue';
+// import CarouselSlider from './components/CarouselSlider.vue';
 
 export default {
   name: 'App',
   components: {
     ReviewPage,
-    CarouselSlider
+    // CarouselSlider
   },
 
   data() {
