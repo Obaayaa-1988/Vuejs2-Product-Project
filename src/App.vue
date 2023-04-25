0b6e15ca9f1b3d01e4bb4f232783b0ac277748d2
Obaayaa-1988 <ashbella333@gmail.com>
2023-04-25 13:45:16 +0000

    removed parentesis from shipping variable

--- src/App.vue
+++ src/App.vue
@@ -191,7 +191,7 @@ export default {
     },
 
     totalItemShipping() {
-      return this.sum = this.totalProductAdded() + (this.shipping);
+      return this.sum = this.totalProductAdded() + this.shipping;
     },
 
 
